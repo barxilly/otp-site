8b57a61bed107ddca7d96f4233fb536bf0e9e1ae
Ben Smith <ben@barxsmith.sbs>
2024-11-18 16:39:03 +0000

    A

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,20 @@ import { TOTP } from 'totp-generator'
 import { FaFile, FaQrcode } from 'react-icons/fa'
 import { FaBarsStaggered } from 'react-icons/fa6'
 
-function App() {
-  const qr = <FaQrcode className='qr-icon' onClick={() => {
+function qrF() {
+  console.log('qr')
+}
 
-  }} />
+function generate() {
+  console.log('generate')
+}
+
+function example() {
+  console.log('example')
+}
+
+function App() {
+  const qr = <FaQrcode className='qr-icon' onClick={qrF} />
 
   return (
     <>
@@ -16,29 +26,27 @@ function App() {
           <Stack className='app-stack'>
             <Text className='title'>WebOTP</Text>
             <Text className='subtitle'>Sleek and Secure TOTP Generator</Text>
-            <TextInput required placeholder='Secret Key' rightSection={qr} />
+            <TextInput required type='password' placeholder='Secret Key' rightSection={qr} />
             <TextInput placeholder='Label' />
             <TextInput placeholder='Issuer' />
-            <Button>Generate</Button>
-            <Flex justify='center'>
-
-            </Flex>
-            <Popover position="top" opened withArrow>
-              <Popover.Target>
-                <ActionIcon variant="outline" color="rgba(0,0,0,1)" size="xl" radius="xl" aria-label="Settings" className='acb'>
-                  <FaBarsStaggered />
-                </ActionIcon>
-              </Popover.Target>
-              <Popover.Dropdown style={{ cursor: 'default', userSelect: 'none' }}>
-                Add Example Items
-              </Popover.Dropdown>
-            </Popover>
+            <Button onClick={generate}>Generate</Button>
           </Stack>
+          <Popover position="top" opened withArrow>
+            <Popover.Target>
+              <ActionIcon variant="outline" color="rgba(0,0,0,1)" size="xl" radius="xl" aria-label="Settings" className='acb' onClick={example}>
+                <FaBarsStaggered />
+              </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown style={{ cursor: 'default', userSelect: 'none' }}>
+              Add Example Item
+            </Popover.Dropdown>
+          </Popover>
         </Center>
-
       </main >
     </>
   )
 }
+
+
 
 export default App
